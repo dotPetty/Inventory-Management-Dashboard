639e5905ff68055b2b9fc3d178e64bca74362f41
dotPetty <atjordanpetty@gmail.com>
2025-02-06 12:43:22 -0500

    Initial Sidebar setup

--- client/src/app/(components)/Sidebar/Sidebar.tsx
+++ client/src/app/(components)/Sidebar/Sidebar.tsx
@@ -1,9 +1,43 @@
+'use client'
+import { Menu } from 'lucide-react'
 import React from 'react'
 
 const Sidebar = () => {
   return (
     <div>
-      <h1>Sidebar</h1>
+        {/* Top Logo */}
+      <div className='flex gap-3 justify-between md:justify-normal items-center pt-8 px-5'>
+            <div>
+                Logo
+            </div>
+
+            <h1 className='font-extrabold text-2xl'>
+                DOTSTOCK
+            </h1>
+        
+            <button
+                className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100'
+                onClick={() => {}}
+            >
+                <Menu 
+                    className='w-4 h-4'
+                />
+            </button>
+        </div>
+
+        {/* Links */}
+        <div className='flex-grow mt-8'>
+            <h1>
+                Links
+            </h1>
+        </div>
+
+        {/* Footer */}
+        <div>
+            <p className='text-center text-xs text-gray-500'>
+                &copy; 2025 DOTSTOCK
+            </p>
+        </div>
     </div>
   )
 }
