9610f6e37f621905c7f1cf8b0982de5fa578df73
dotPetty <atjordanpetty@gmail.com>
2025-02-06 19:33:02 -0500

    completed Sidebar ui

--- client/src/app/(components)/Sidebar/Sidebar.tsx
+++ client/src/app/(components)/Sidebar/Sidebar.tsx
@@ -1,8 +1,42 @@
 'use client'
 import { useAppDispatch, useAppSelector } from '@/app/(redux)/Redux';
 import { setIsSidebarCollapsed } from '@/state';
-import { Menu } from 'lucide-react';
+import { Archive, CircleDollarSign, Clipboard, Icon, Layout, LucideIcon, Menu, SlidersHorizontal, User } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
+
+// Creating Sidebar Links.
+// 'pathname': grap the path of the URL that we're at.
+// 'isActive': determine if the link URL is active so we can know what page we are on and determine what to highlight
+interface SidebarLinkProps {
+    href: string;
+    icon: LucideIcon;
+    label: string;
+    isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+    href,
+    icon: Icon,
+    label,
+    isCollapsed
+}: SidebarLinkProps) => {
+    const pathname = usePathname();
+    const isActive = pathname === href || (pathname === '/' && href === '/dashboard');
+
+    return (
+        <Link href={href}>
+            <div className={`cursor-pointer flex items-center ${isCollapsed ? 'justify-center py-4' : 'justify-start px-8 py-4'} hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${isActive ? 'bg-blue-200 text-white' : ''}`}>
+                <Icon className='w-6 h-6 !text-gray-700' />
+
+                <span className={`${isCollapsed ? 'hidden' : 'block'} font-medium text-gray-700`}>
+                    {label}
+                </span>
+            </div>
+        </Link>
+    )
+}
 
 const Sidebar = () => {
   // Addidng state as we need
@@ -16,7 +50,7 @@ const Sidebar = () => {
 
   // Classes to apply different setting
   const sidebarClassNames = `fixed flex flex-col ${
-    isSidebarCollapsed ? 'w-0' : 'w-72 md:w-64'
+    isSidebarCollapsed ? 'w-0 md:w-16' : 'w-72 md:w-64'
   } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
 
   return (
@@ -27,7 +61,7 @@ const Sidebar = () => {
                 Logo
             </div>
 
-            <h1 className='font-extrabold text-2xl'>
+            <h1 className={`${isSidebarCollapsed ? 'hidden' : 'block'} font-extrabold text-2xl`}>
                 DOTSTOCK
             </h1>
         
@@ -43,13 +77,46 @@ const Sidebar = () => {
 
         {/* Links */}
         <div className='flex-grow mt-8'>
-            <h1>
-                Links
-            </h1>
+            <SidebarLink 
+                href='/dashboard'
+                icon={Layout}
+                label='Dashboard'
+                isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink 
+                href='/inventory'
+                icon={Archive}
+                label='Inventory'
+                isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink 
+                href='/products'
+                icon={Clipboard}
+                label='Products'
+                isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink 
+                href='/users'
+                icon={User}
+                label='Users'
+                isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink 
+                href='/settings'
+                icon={SlidersHorizontal}
+                label='Settings'
+                isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink 
+                href='/expenses'
+                icon={CircleDollarSign}
+                label='Expenses'
+                isCollapsed={isSidebarCollapsed}
+            />
         </div>
 
         {/* Footer */}
-        <div>
+        <div className={`${isSidebarCollapsed ? 'hidden' : 'block'} mb-10`}>
             <p className='text-center text-xs text-gray-500'>
                 &copy; 2025 DOTSTOCK
             </p>
