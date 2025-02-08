1c5aa960eec045f39844f3ebf2cbdde5cfef9f2e
dotPetty <atjordanpetty@gmail.com>
2025-02-08 14:30:47 -0500

    Darkmode toggle function

--- client/src/app/(components)/Navbar/Navbar.tsx
+++ client/src/app/(components)/Navbar/Navbar.tsx
@@ -1,20 +1,27 @@
 'use client';
 
-import { Bell, Menu, Settings, Sun } from 'lucide-react';
+import { Bell, Menu, Moon, Settings, Sun } from 'lucide-react';
 import { useAppDispatch, useAppSelector } from '@/app/(redux)/Redux';
-import { setIsSidebarCollapsed } from '@/state';
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/state';
 import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
  // Addidng state as we need
-  const dispath = useAppDispatch();
+  const dispatch = useAppDispatch();
+  // Sidebar state.
   const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+  // Darkmode state.
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
   
-  // Toggle Sidebar, invert Sidebar Collapse anytime this function is triggered.
+  // Sidebar toggle function
   const toggleSidebar = () => {
-    dispath(setIsSidebarCollapsed(!isSidebarCollapsed));
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
   };
+  // Darkmode toggle function
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+   }
 
   return (
     <div className='flex justify-between items-center w-full mb-7'>
@@ -33,7 +40,7 @@ const Navbar = () => {
                 <input 
                     type='search'
                     placeholder='Search groups and products...'
-                    className='pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500'
+                    className='pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500'
                 />
 
                 <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
@@ -50,12 +57,10 @@ const Navbar = () => {
             <div className='hidden md:flex justify-between items-center gap-5'>
                 <div>
                     <button
-                        onClick={() => {}}
+                        onClick={toggleDarkMode}
                     >
-                        <Sun 
-                            className='cursor-pointer text-gray-500'
-                            size={24}
-                        />
+                        {isDarkMode ? (<Moon className='cursor-pointer text-gray-500' size={24}/>) : (<Sun className='cursor-pointer text-gray-500' size={24}/>)}
+                        
                     </button>
                 </div>
                 
