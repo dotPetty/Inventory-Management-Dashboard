852612ecd865b12a6d4c3ebf8d4d615593b04525
dotPetty <atjordanpetty@gmail.com>
2025-02-08 20:35:44 -0500

    setup database, prisma schemas and db connection

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -4,6 +4,10 @@ import fs from "fs";
 import path from "path";
 const prisma = new PrismaClient();
 
+// seed data, delete data, and upload the data that we have to our sql database.
+
+
+// delete data function (first deletes any pre-existing data in database incase you update your seed data in any way).
 async function deleteAllData(orderedFileNames: string[]) {
   const modelNames = orderedFileNames.map((fileName) => {
     const modelName = path.basename(fileName, path.extname(fileName));
@@ -23,9 +27,12 @@ async function deleteAllData(orderedFileNames: string[]) {
   }
 }
 
+// function to geab seed data from the seedData directory.
+
 async function main() {
   const dataDirectory = path.join(__dirname, "seedData");
-
+  
+  // figure out the file names (order matters for foreign id connection: ids are created in sequential order).
   const orderedFileNames = [
     "products.json",
     "expenseSummary.json",
@@ -37,20 +44,24 @@ async function main() {
     "expenses.json",
     "expenseByCategory.json",
   ];
-
+  
+  // delete data (logic above).
   await deleteAllData(orderedFileNames);
 
+
+  // grab each file, get the correct name with error handling, and create model schemas.
   for (const fileName of orderedFileNames) {
     const filePath = path.join(dataDirectory, fileName);
     const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
     const modelName = path.basename(fileName, path.extname(fileName));
     const model: any = prisma[modelName as keyof typeof prisma];
-
+    
+    // error handling.
     if (!model) {
       console.error(`No Prisma model matches the file name: ${fileName}`);
       continue;
     }
-
+    // create model schemas and pass data into each.
     for (const data of jsonData) {
       await model.create({
         data,
