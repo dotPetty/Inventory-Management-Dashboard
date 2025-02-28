41236035911f5bd521f77d3a4d01b92c027ce721
dotPetty <atjordanpetty@gmail.com>
2025-02-28 13:26:58 -0500

    Expense Summary ui

--- client/src/app/(components)/dashboard/ExpenseSummaryCard.tsx
+++ client/src/app/(components)/dashboard/ExpenseSummaryCard.tsx
@@ -1,6 +1,7 @@
 import { ExpenseByCategorySummary, useGetDashboardMetricsQuery } from '@/state/api';
+import { TrendingUp } from 'lucide-react';
 import React from 'react';
-import { PieChart, ResponsiveContainer } from 'recharts';
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts';
 
 type ExpenseSums = {
     [category: string]: number;
@@ -11,6 +12,8 @@ const colors = ["#00C49F", "#0088FE", "#FFBB28"];
 const ExpenseSummaryCard = () => {
   // Fetching data from Redux Toolkit
   const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+
+  const expenseSummary = dashboardMetrics?.expenseSummary[0];
 
   // Formatting summary data.
   const expenseByCategorySummary =
@@ -33,6 +36,12 @@ const ExpenseSummaryCard = () => {
           value,
         })
       );
+
+      const totalExpenses = expenseCategories.reduce(
+        (acc, category: { value: number }) => acc + category.value,
+        0
+      );
+      const formattedTotalExpenses = totalExpenses.toFixed(2);
 
 
   return (
@@ -76,7 +85,50 @@ const ExpenseSummaryCard = () => {
                         </Pie>
                     </PieChart>
                 </ResponsiveContainer>
+
+                <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center basis-2/5 '>
+                    <span className='font-bold text-xl'>
+                      ${formattedTotalExpenses}
+                    </span>
+                </div>
             </div>
+
+            {/* LABELS */}
+            <ul className='flex flex-col justify-around items-center xl:items-start py-5 gap-3'>
+              {expenseCategories.map((entry, index) => (
+                <li
+                 key={`legend-${index}`}
+                 className='flex items-center text-xs'
+                >
+                  <span className='mr-2 w-3 h-3 rounded-full' style={{backgroundColor: colors[index % colors.length]}}>
+
+                  </span>
+                  {entry.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* FOOTER */}
+          <div>
+            <hr />
+            {expenseSummary && (
+              <div className='mt-3 flex justify-between items-center px-7 mb-4'>
+                <div className='pt-2'>
+                  <p className='text-sm'>
+                    Average:{" "}
+                    <span className='font-semibold'>
+                      ${expenseSummary.totalExpenses.toFixed(2)}
+                    </span>
+                  </p>
+                </div>
+
+                <span className='flex items-center mt-2'>
+                  <TrendingUp className='mr-2 text-green-500' />
+                  30%
+                </span>
+              </div>
+            )}
           </div>
         </>
       )};
